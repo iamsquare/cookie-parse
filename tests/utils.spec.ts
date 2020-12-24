02d7815202dc276ea27b9a739e61440555901e49
Marco Cesi <sq@iamsquare.it>
2020-12-24 13:46:50 +0100

    Add arrayToCookie tests Add husky and lint-staged configuration

--- tests/utils.spec.ts
+++ tests/utils.spec.ts
@@ -1,10 +1,51 @@
-import { splitTrimLine } from '../src/utils';
+import { splitTrimLine, arrayToCookie } from '../src/utils';
 
 describe('splitTrimLine', () => {
-  test('Without trim', () => {
+  it('Should generate a valid array from a string without extra whitespace', () => {
     expect(splitTrimLine('a\tb\tc')).toStrictEqual(['a', 'b', 'c']);
   });
-  test('With trim', () => {
+  it('Should generate a valid array from a string with extra whitespace', () => {
     expect(splitTrimLine('  a  \tb  \t  c')).toStrictEqual(['a', 'b', 'c']);
+  });
+});
+
+describe('arrayToCookie', () => {
+  it('Should generate a valid ParsedCookie', () => {
+    expect(arrayToCookie(['.test.dev', 'TRUE', '/', 'FALSE', '1629107116', 'key1', 'value1'])).toStrictEqual({
+      name: 'key1',
+      value: 'value1',
+      domain: '.test.dev',
+      crossDomain: true,
+      path: '/',
+      httpOnly: false,
+      https: false,
+      expire: 1629107116
+    });
+  });
+
+  it('Should generate a valid ParsedCookie with httpOnly domain', () => {
+    expect(arrayToCookie(['#HttpOnly_.test.dev', 'TRUE', '/', 'FALSE', '1629107116', 'key1', 'value1'])).toStrictEqual({
+      name: 'key1',
+      value: 'value1',
+      domain: '.test.dev',
+      crossDomain: true,
+      path: '/',
+      httpOnly: true,
+      https: false,
+      expire: 1629107116
+    });
+  });
+
+  it('Should generate a valid ParsedCookie without expiration date', () => {
+    expect(arrayToCookie(['#HttpOnly_.test.dev', 'TRUE', '/', 'FALSE', '0', 'key1', 'value1'])).toStrictEqual({
+      name: 'key1',
+      value: 'value1',
+      domain: '.test.dev',
+      crossDomain: true,
+      path: '/',
+      httpOnly: true,
+      https: false,
+      expire: 0
+    });
   });
 });
